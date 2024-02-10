bc3211434223df5c2ca1b333043d54c715eca781
EulerCode <155048861+EulerCode@users.noreply.github.com>
2024-02-10 13:51:50 -0300

    Update globals.js

--- public/globals.js
+++ public/globals.js
@@ -5,11 +5,11 @@ var PaginaWeb = "zona-sneakers.vercel.app";
 var Email = "zona.sneakers@gmail.com";
 var Direccion = "1245, Av. San Martín 1233, M5500 Mendoza"; // 
 var ImporteMP=0.15;
-var cbu="0000013000032111981657";
-var Ncuenta="11198165";
+var cbu="0000076500000022318161";
+var Ncuenta="43749280";
 var alias="xxxxxxx";
-var titular= "Echevarria Fernando";
-var cuil="27-37883880-6";
+var titular= "Gil Franco Luciano";
+var cuil="20-43749280-9";
 
 var linkImgMail = "https://i.ibb.co/w7MM5LJ/email.jpg";
 var linkMP="https://852u.adj.st/home?adjust_t=mqo65e0&adjust_deeplink=mercadopago%3A%2F%2Fhome&adjust_label=account-stickybutton-notlogged&engagement_type=fallback_click";
